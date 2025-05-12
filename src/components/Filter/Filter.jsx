e8bdd30a97086dfc7c4729d313391658c08f81a3
ddumni0227 <ddum_ni0227@kakao.com>
2025-05-12 15:37:04 +0900

    코드정리

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -11,24 +11,37 @@ const FilterBar = ({
 }) => {
   return (
     <div className="filter_bar">
-      {/* 타입 필터 */}
-      <select value={selectedType} onChange={(e) => setSelectedType(e.target.value)}>
-        {["전체", "fire", "water", "grass", "electric", "bug", "normal", "poison", "flying"].map((type) => (
+      <select
+        value={selectedType}
+        onChange={(e) => setSelectedType(e.target.value)}
+      >
+        {[
+          "전체",
+          "fire",
+          "water",
+          "grass",
+          "electric",
+          "bug",
+          "normal",
+          "poison",
+          "flying",
+        ].map((type) => (
           <option key={type} value={type}>
             {type === "전체" ? "전체" : type}
           </option>
         ))}
       </select>
 
-      {/* 이름(가나다) 정렬 */}
       <select value={nameSort} onChange={(e) => setNameSort(e.target.value)}>
         <option value="id">번호순</option>
         <option value="korean_asc">가나다순</option>
         <option value="korean_desc">가나다 역순</option>
       </select>
 
-      {/* 경험치/능력치 통합 정렬 */}
-      <select value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+      <select
+        value={sortOption}
+        onChange={(e) => setSortOption(e.target.value)}
+      >
         <option value="">경험치/능력치</option>
         <option value="exp_desc">경험치 높은 순</option>
         <option value="exp_asc">경험치 낮은 순</option>
@@ -44,5 +57,3 @@ const FilterBar = ({
 };
 
 export default FilterBar;
-
-
